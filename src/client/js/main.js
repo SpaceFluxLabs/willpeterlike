8786013ba4e2f2d4e4a53dfffe687f9e89627d2b
Wilbur Yu <yuwilbur@gmail.com>
2014-04-26 23:59:19 -0700

    Moved source to src and added boilerplate version of phaser

---
+++ src/client/js/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800,600,Phaser.CANVAS, 'phaser', {preload: preload, create: create, update: update});
+
+function preload() {
+	// Stuff happens here before the canvas is created.
+}
+
+function create() {
+	// Stuff happens here the instance the canvas is created.
+}
+
+function update() {
+	// This function gets called per certain time interval. How often, need to check documentation
+}
