1fd807bfa4c853d9820372e6be5bea7dafc5c71d
Jim Wang <jim.gs.wang@gmail.com>
2014-04-27 22:00:46 -0700

    add firebase script

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -1,6 +1,8 @@
 var game = new Phaser.Game(800,600,Phaser.CANVAS, 'phaser', {preload: preload, create: create, update: update});
 var input;
 var circle;
+var firebase = new Firebase('https://jnks031h2o4.firebaseio-demo.com');
+var prevPoint = new Phaser.Point();
 
 function preload() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -12,20 +14,40 @@ function create() {
 	input = new Input(game);
   circle = game.add.sprite(400,300,'circleImg'); // (400,300) is the position
   game.physics.enable(circle, Phaser.Physics.ARCADE);
+
+  firebase.on('child_added', function(snapshot) {
+      var data = snapshot.val();
+      var point = new Phaser.Point(data.x, data.y);
+
+      console.log(circle);
+      circle.body.velocity = point;
+  });
+
 }
 
 function update() {
-  circle.body.velocity.setTo(0,0);
+  //circle.body.velocity.setTo(0,0);
+  var point = new Phaser.Point(),
+      speed = 200;
+
 	if (input.upKey.isDown) {
-		circle.body.velocity.y -= 200;
+		point.y -= speed;
 	}
 	if (input.downKey.isDown) {
-		circle.body.velocity.y += 200;
+		point.y += speed;
 	}
 	if (input.leftKey.isDown) {
-		circle.body.velocity.x -= 200;
+	        point.x -= speed;
 	}
 	if (input.rightKey.isDown) {
-		circle.body.velocity.x += 200;
+		point.x += speed;
 	}
+
+        circle.body.velocity = point;
+        if(prevPoint.x !== point.x || prevPoint.y !== point.y) {
+            firebase.push(point);
+        }
+
+        prevPoint = point;
 }
+
