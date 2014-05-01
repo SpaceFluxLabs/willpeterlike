6aa7f34172848e2ea04a2482eadd16c07d5f7437
Jim Wang <jim.gs.wang@gmail.com>
2014-05-01 07:34:50 -0700

    fix naming error in Player.js

--- src/client/js/Player.js
+++ src/client/js/Player.js
@@ -14,26 +14,27 @@ var Player = (function(Phaser) {
       , right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
       , speed = 200;
 
-    up.onDown.add(this.velocity.bind(0, -speed), this);
-    up.onUp.add(this.velocity.bind(0, speed), this);
-    down.onDown.add(this.velocity.bind(0, speed), this);
-    down.onUp.add(this.velocity.bind(0, -speed), this);
+    up.onDown.add(this.setVelocity.bind(0, -speed), this);
+    up.onUp.add(this.setVelocity.bind(0, speed), this);
+    down.onDown.add(this.setVelocity.bind(0, speed), this);
+    down.onUp.add(this.setVelocity.bind(0, -speed), this);
 
-    right.onDown.add(this.velocity.bind(speed, 0), this);
-    right.onUp.add(this.velocity.bind(-speed, 0), this);
-    left.onDown.add(this.velocity.bind(-speed, 0), this);
-    left.onUp.add(this.velocity.bind(speed, 0), this);
+    right.onDown.add(this.setVelocity.bind(speed, 0), this);
+    right.onUp.add(this.setVelocity.bind(-speed, 0), this);
+    left.onDown.add(this.setVelocity.bind(-speed, 0), this);
+    left.onUp.add(this.setVelocity.bind(speed, 0), this);
+
   }
 
 
-  Player.prototype.velocity = function(x, y) {
+  Player.prototype.setVelocity = function(x, y) {
 
     this.velocity.x += x;
     this.velocity.y += y;
 
     this.firebase.push({
       id: this.id,
-      point: this.velocity;
+      point: this.velocity
     });
 
   };
