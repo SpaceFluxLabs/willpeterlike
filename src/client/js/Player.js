f09fce67c89d5e2110f11507c986f18a526b2d8d
Jim Wang <jim.gs.wang@gmail.com>
2014-05-01 06:57:35 -0700

    player class

---
+++ src/client/js/Player.js
@@ -0,0 +1,44 @@
+var Player = (function(Phaser) {
+
+  function Player(id, game, firebase) {
+
+    this.id = id;
+    this.firebase = firebase;
+    this.velocity = new Phaser.Point();
+
+    // Register keyboard events
+    
+    var up = game.input.keyboard.addKey(Phaser.Keyboard.UP)
+      , down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+      , left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+      , right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+      , speed = 200;
+
+    up.onDown.add(this.velocity.bind(0, -speed), this);
+    up.onUp.add(this.velocity.bind(0, speed), this);
+    down.onDown.add(this.velocity.bind(0, speed), this);
+    down.onUp.add(this.velocity.bind(0, -speed), this);
+
+    right.onDown.add(this.velocity.bind(speed, 0), this);
+    right.onUp.add(this.velocity.bind(-speed, 0), this);
+    left.onDown.add(this.velocity.bind(-speed, 0), this);
+    left.onUp.add(this.velocity.bind(speed, 0), this);
+  }
+
+
+  Player.prototype.velocity = function(x, y) {
+
+    this.velocity.x += x;
+    this.velocity.y += y;
+
+    this.firebase.push({
+      id: this.id,
+      point: this.velocity;
+    });
+
+  };
+
+  return Player;
+
+})(Phaser);
+
