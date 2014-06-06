90aa634f62cc8ee9d3b2173782840ebf0754a1a3
Wilbur Yu <yuwilbur@gmail.com>
2014-06-06 01:35:18 -0700

    Moved Bullet related functions from Line to Bullet

--- src/client/js/Bullet.js
+++ src/client/js/Bullet.js
@@ -5,6 +5,26 @@ define(['Line', 'Lodash'], function(Line, _) {
         Line.call(this, game, firebase, x, y);
     }
 
+    Bullet.prototype.setVelocity = function(x, y) {
+      this.velocity.x = x;
+      this.velocity.y = y;
+    }
+
+    Bullet.prototype.getUnitVelocity = function() {
+      var absVelocity = Math.sqrt(Math.pow(this.velocity.x, 2) + Math.pow(this.velocity.y,2));
+      if (absVelocity === 0) {
+        return {
+          x : 0,
+          y : 0
+        }
+      } else {
+        return {
+          x : this.velocity.x / absVelocity,
+          y : this.velocity.y / absVelocity
+        }
+      }
+    }
+
     // Inherit all of Line's prototypal properties into Bullet
     Bullet.prototype = _.create(Line);
 
--- src/client/js/Line.js
+++ src/client/js/Line.js
@@ -2,15 +2,20 @@ define(['Phaser'], function(Phaser) {
   'use strict';
 
   function Line(game, firebase, x, y) {
-    this.type = "Line";
     this.firebase = firebase;
     this.position = new Phaser.Point(x,y);
     this.velocity = new Phaser.Point(0,0);
     this.lineLength = 50;
+
+    // Creates bitmap with dimention lineLength x lineLength
     this.bitmap = game.add.bitmapData(this.lineLength,this.lineLength);
 
+    // Adds the bitmap to a sprite at location (x,y)
     this.sprite = game.add.sprite(x, y, this.bitmap);
+
+    // Sets the sprite anchor to center instead of corner
     this.sprite.anchor.setTo(0.5,0.5);
+
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 
     this.sprite.body.collideWorldBounds = false;
@@ -22,26 +27,6 @@ define(['Phaser'], function(Phaser) {
         position: this.position,
         velocity: this.velocity
     });
-  }
-
-  Line.prototype.setVelocity = function(x, y) {
-    this.velocity.x = x;
-    this.velocity.y = y;
-  }
-
-  Line.prototype.getUnitVelocity = function() {
-    var absVelocity = Math.sqrt(Math.pow(this.velocity.x, 2) + Math.pow(this.velocity.y,2));
-    if (absVelocity === 0) {
-      return {
-        x : 0,
-        y : 0
-      }
-    } else {
-      return {
-        x : this.velocity.x / absVelocity,
-        y : this.velocity.y / absVelocity
-      }
-    }
   }
 
   Line.prototype.draw = function() {
