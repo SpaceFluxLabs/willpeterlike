e048dd314fe9a5f4ff398f76cd14659d890b41fe
Wilbur Yu <yuwilbur@gmail.com>
2014-06-15 18:00:43 -0700

    Added Ammo test cases

---
+++ test/client/js/AmmoSpec.js
@@ -0,0 +1,40 @@
+define(['Ammo',
+    'Mock/Game',
+    'Mock/Firebase',
+    'Lodash'
+    ],
+    function(Ammo,
+      MockGame,
+      MockFirebase,
+      _
+    ){
+  describe('Ammo class', function() {
+    var mockedGame,
+        mockedFirebase,
+        ammo,
+        x = 1,
+        y = 2,
+        direction = 3,
+        speed = 4
+    ;
+
+    beforeEach(function() {
+      jasmine.clock().install();
+      mockedGame = _.clone(MockGame, true);
+      mockedFirebase = _.clone(MockFirebase, true);
+
+      ammo = new Ammo(mockedGame, mockedFirebase, x, y, direction, speed);
+    });
+
+    afterEach(function() {
+      jasmine.clock().uninstall();
+    });
+
+    it('should instantiate', function() {
+      expect(ammo.position.x).toBe(x);
+      expect(ammo.position.y).toBe(y);
+      expect(ammo.direction).toBe(direction);
+      expect(ammo.speed).toBe(speed);
+    });
+  });
+})
